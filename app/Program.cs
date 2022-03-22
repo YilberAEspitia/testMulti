5020c5df8dfce3a64757c448d4f89673424d8cff
YilberEspitia <yilber.espitia@grantitan.com>
2022-03-22 07:59:26 -0500

    Agregar archivos de proyecto.

---
+++ app/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            Console.WriteLine("Ingrese el primer número");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(multiplo.Util.EsMultiplo(a, b));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ multiplo/Util.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace multiplo
+{
+    public class Util
+    {
+        public static string EsMultiplo(int a, int b)
+        {
+            if(a % b ==0)
+            {
+                return string.Format("El número a: {0}, es múltiplo del número b: {1}", a, b);
+            }
+            else
+            {
+                return string.Format("El número a: {0}, no es múltiplo del número b: {1}", a, b);
+            }
+        }
+    }
+}
